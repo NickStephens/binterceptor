d7c75719a95c30da4f0780c66194d2a4b058328b
Nick Stephens <nick.stephens93@gmail.com>
2013-08-20 21:37:14 -0700

    Intercept packets and make decision from user

--- binterceptor.py
+++ binterceptor.py
@@ -12,29 +12,57 @@ def main():
     sockfd.bind(("0.0.0.0", 13074))
     sockfd.listen(1)
     client, desc = sockfd.accept()
+    print "Incoming clientection from", desc 
 
     # read from config and get forward
     # target and port
 
-    server = socket.create_connection(("localhost", 80)) 
+    server = socket.create_connection(("www.google.com", 80)) 
+    interceptionLoop(client, server)
 
-    print "Incoming clientection from", desc 
+def interceptionLoop(clientSock, serverSock):
+    """ the BINTERCEPTOR's dark heart """
     while True:
-        cdata = client.recv(2048)
+        cdata = clientSock.recv(2048)
         if (not cdata): break
-        print "[client] sending ..."
-        hex = converter.convert(cdata)
-        print hex
         # edit, forward, or drop? [F/e/d] 
-        server.send(cdata)
-        
+        if (not prompt(cdata, serverSock, "client")):
+            continue
         # send to server if forwarded is selected
-        sdata = server.recv(2048)
+        sdata = serverSock.recv(2048)
         if (not sdata): break
-        print "[server] sending ..."
-        hex = converter.convert(sdata)
-        print hex
+        prompt(sdata, clientSock, "server")
         # edit, forward, or drop?
-        
+
+def prompt(data, targetSock, targetName):
+    """ prompts user for a decision regarding the data captured returns False if the data was dropped, True otherwise """
+
+    print "[{}] sending ...".format(targetName)
+    prettyhex = converter.convert(data)
+    print prettyhex
+
+    decision = raw_input("edit, forward, or drop? [F/e/d] ").upper()
+
+    if (decision == "F"):
+        forward(data, targetSock)
+    elif (decision == "E"):
+        edit(data, targetSock, targetName)
+    elif (decision == "D"):
+        print "dropped" 
+        return False 
+    else:
+        forward(targetSock, data)
+
+    return True
+
+def forward(data, targetSock):
+    targetSock.send(data)
+
+def edit(data, targetSock, targetName):
+    print "edit has been called"
+    # write contents to a temporary file in tmp"
+    # open up $EDITOR on that file
+    # convert file to binary and prompt new data
+    prompt(data, targetSock, "you")
 
 main()
